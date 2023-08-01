5bca8d8b1d6bbd32a578cea9fc7b9849eb659f59
Paul Cantalupo <pcantalupo@gmail.com>
2023-08-01 11:00:43 -0400

    adding seurat_info.R script

---
+++ R/seurat_info.R
@@ -0,0 +1,25 @@
+pacman::p_load('optparse')
+
+##################### Options ########################
+option_list=list(
+  make_option("--seuratrds", default="", type="character", help="Seurat object [required; default: %default]")
+)
+opt_parser <- OptionParser(option_list=option_list)
+opts <- parse_args(opt_parser)
+
+if (opts$seuratrds == "") {
+  message("\nError: --seuratrds parameter is required.")
+  print_help(opt_parser)
+  quit(status=1)
+}
+
+pacman::p_load('Seurat', 'sct')
+
+message("Seurat version: ", packageVersion('Seurat'))
+
+seurat = readRDS(opts$seuratrds)
+SeuratInfo(seurat)
+
+message("\n")
+FindIdentLabel(seurat)
+
