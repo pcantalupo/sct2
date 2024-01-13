8fbe8474b58633da9f8ae2386e15c5219929755f
Paul Cantalupo <pcantalupo@gmail.com>
2024-01-13 16:34:33 -0500

    saving Seurat pbmc_small object as a SingleCellExperiment object

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' PBMC Seruat object
+#' PBMC Seurat object
 #'
 #' This is the pbmc_small Seurat object from the Seurat package
 #'
@@ -15,4 +15,23 @@
 #' @source https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k
 #' @references Seurat package https://satijalab.org/seurat/
 "pbmc_small"
+
+#' PBMC SCE object
+#'
+#' This is the pbmc_small Seurat object from the Seurat package converted
+#' to a SingleCellExperiment object
+#'
+#' @format
+#' Seurat object with 230 rows/genes and 80 columns/cells
+#' \describe{
+#'   \item{nCount_RNA}{Number of UMI counts in the cell}
+#'   \item{nFeature_RNA}{Number of genes detected in the cell}
+#'   \item{RNA_snn_res.0.8}{Cluster identity using resolution 0.8}
+#'   \item{RNA_snn_res.1}{Cluster identity using resolution 1}
+#'   \item{letter.idents}{Sample id?}
+#'   \item{groups}{Group id}
+#' }
+#' @source https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k
+#' @references Seurat package https://satijalab.org/seurat/
+"pbmc_small_sce"
 
--- data-raw/pbmc_small.R
+++ data-raw/pbmc_small.R
@@ -4,3 +4,7 @@ dir.create("inst/extdata", showWarnings = FALSE)
 saveRDS(pbmc_small, "inst/extdata/pbmc_small.rds")
 usethis::use_data(pbmc_small, overwrite = TRUE)
 
+pbmc_small_sce = as.SingleCellExperiment(pbmc_small)
+saveRDS(pbmc_small_sce, "inst/extdata/pbmc_small_sce.rds")
+usethis::use_data(pbmc_small_sce, overwrite = TRUE)
+
