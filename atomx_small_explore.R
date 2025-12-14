31a00e49c11f05055cb31613499be7ad17d1f026
Paul Cantalupo <pcantalupo@gmail.com>
2025-12-14 12:12:26 -0500

    updated

--- atomx_small_explore.R
+++ atomx_small_explore.R
@@ -1,16 +1,15 @@
-pacman::p_load(nvutils, qs2, Seurat, tidyverse)
 
 seurat = readRDS("atomx_small.rds")
 # RNA assay has data slot BUT it is NOT normalized
 #     LayerData(seurat, layer = "data", assay = "RNA")[1:10,1:5]
 # MettoLungBoneRNA assay contains the normalized data
 #     LayerData(seurat, layer = "data", assay = "Met.to.Lung.Bone_Normalization.RNA.1_1")[1:10,1:5]
-DefaultAssay(seurat) = "Met.to.Lung.Bone_Normalization.RNA.1_1"
+Seurat::DefaultAssay(seurat) = "Met.to.Lung.Bone_Normalization.RNA.1_1"
 
 niches = "Met.to.Lung.Bone_Neighborhood.Analysis.1_1_assignments"
 celltypes = "RNA_Met.to.Lung.Bone_Cell.Typing.InSituType.1_1_clusters"
 clusters = "Met.to.Lung.Bone_Neighbor.network.expression.space.1_1_cluster_Met.to.Lung.Bone_Leiden.Clustering.1_1"
-Idents(seurat) = celltypes
+Seurat::Idents(seurat) = celltypes
 
 start <- Sys.time()
 summarize_seurat(seurat)
